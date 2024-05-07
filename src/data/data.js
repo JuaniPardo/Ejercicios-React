17f4b1bc5193671421afc363cb7e1899f93b80bc
JuaniPardo <juani.pardo@icloud.com>
2024-05-07 02:34:22 -0300

    [FEAT]: Ejercicios 1, 2, 3 y 4.

---
+++ src/data/data.js
@@ -0,0 +1,7 @@
+export const productos = [
+    {ID: 1, nombre: "Lápiz", precio: 500},
+    {ID: 2, nombre: "Cuaderno", precio: 1200},
+    {ID: 3, nombre: "Borrador", precio: 300},
+    {ID: 4, nombre: "Tijeras", precio: 2000},
+    {ID: 5, nombre: "Pegamento", precio: 1500}
+];
